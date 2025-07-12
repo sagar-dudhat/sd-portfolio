4a1cb1ff1ecfa908ad7b7b38ed41502a4dc46f00
RainstreamwebSD <sagard@rainstreamweb.com>
2025-07-12 11:40:26 +0530

    [Remove] : comment out pages to fix the mobile app issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About />
+        {/*<About />
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        <Feedbacks />*/}
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
